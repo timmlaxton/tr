a6af527df59abf7aa6c6042fa13b468678750490
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-09 09:38:09 +0100

    added form and todolist component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo-List</h1>
+        <Form />
+      </header>
+    </div>
+  );
 }
 
 export default App;
